0bfa7ce6a32a5677d6b44f3c6dc9df1fb18c48ae
kluless13 <100767970+kluless13@users.noreply.github.com>
2023-08-17 08:17:40 +0530

    questions + changes

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Ordinautz
 
-A repository for updates on all things related to Ordinautz. 
+_A repository for updates on all things related to Ordinautz._
 
-Ordinautz - https://twitter.com/ordinautz
+*Ordinautz - https://twitter.com/ordinautz*
 
 
 For daily updates, follow #notes on Ordinautz discord. For access, DM @kluless_ on twitter or @.kluless on discord.
-Literature for what is being built will be updated soon along with a whitepaper and a roadmap (gitbook optional).
 
-A webhook has been added to connect this repo to discord's #notes.
+Literature for what is being built will be updated soon along with a whitepaper and a roadmap.
 
-31/07/2023 - Version 0.01
+_A webhook has been added to connect this repo to discord's #notes._
 
-15/08/2023 - CLI WL tool being built; will be published this week.
+_31/07/2023_ - Version 0.01 of Knowledge Graph structure.
+
+_15/08/2023_ - CLI WL tool being built; will be published this week.
+
+_16/08/2023_ - CLI WL tool a.k.a `cli-quiz` ready to deploy.
+            Tests will be conducted with a small cohort of 6-8 community members.
+            If the process is smooth, seamless and without any friciton, it shall be published as an `npx` package.
+            
+*Decentralised Knowledge Graph:*
+
+    The Knowledge Graph version 0.01, will be developed by the .bitverse team; we are grateful that we gravitated towards each other to build out the Proof of Concept. The .bitverse team shall help with the development by means of recursive inscriptions.
+
+    A knowledge graph consists of nodes and edges -- the nodes are entities that contain core information, in our case, a node would typically consist of the project's name, some information surrounding it; same with founders and their information. The edge is what defines a relaitonship between two nodes. For example, a node which contains information on a project may be linked to another node which has information about the project or a technology relevant to the project. The nodes here are and the edges are .
+    
+    The POC architecture can be found in the `cli-kg` folder, and was built using Neo4j and cypher query. In order to create a navigation tool, it is important to map out the area first. The intelligence will be provided by Ordinautz, which will turn into a decentralised knowledge graph built on .bitverse: the vision is to create a navigation tool while also imparting some education on the relevant topics.
--- cli-quiz/index.js
+++ cli-quiz/index.js
@@ -55,33 +55,63 @@ async function main() {
     console.log(chalk.bgBlack(chalk.yellow('Welcome to the Ordinautz Quiz!\n')));
 
     const question1 = new Question(
-        "What is the name of the Ordinautz founder?",
-        ["Hantoshi", "Good", "Frogtoshi", "ZKShark", "Danny"],
-        0 // correct answer index
-    );
-
-    const question2 = new Question(
-        "Who is the founder of MetaBRC?",
-        ["Hantoshi", "Good", "Frogtoshi", "ZKShark", "Danny"],
+        "How do you inscribe metadata onto a satoshi?",
+        ["Inception","Inscription","Injection","Email"],
         1 // correct answer index
     );
 
-    const question3 = new Question(
-        "Who is the founder of Bitcoin Frogs?",
-        ["Hantoshi", "Good", "Frogtoshi", "ZKShark", "Danny"],
+    const question2 = new Question(
+        "What secondary layer network is used to scale Bitcoin transactions?",
+        ["Imagine dragons","Thunder","Lightening","Storm"],
         2 // correct answer index
     );
 
+    const question3 = new Question(
+        "What did the OCM team create than enables easy storage of large files on chain?",
+        ["Recursive Inscription","Recursive Injection","Repurposed Inscription","Receive Transmission"],
+        0 // correct answer index
+    );
+
     const question4 = new Question(
-        "Who is the founder of Ordinal Maxi Biz?",
-        ["Hantoshi", "Good", "Frogtoshi", "ZKShark", "Danny"],
-        3 // correct answer index
+        "What is Ordinautz' mission statement?",
+        ["Map out the Ordinal space","Tap out the Ordinal space","Crap on the Ordinal space","Fap to the Ordinal space"],
+        0 // correct answer index
     );
 
     const question5 = new Question(
-        "Who is the founder of Onchain Monkeys?",
-        ["Hantoshi", "Good", "Frogtoshi", "ZKShark", "Danny"],
-        4 // correct answer index
+        "How many satoshis are there in one block?",
+        ["1 million","10 million","100 billion","100 million"],
+        3 // correct answer index
+    );
+
+    const question6 = new Question(
+        "What is the full form of UTXO?",
+        ["Unkept transaction output", "Unspent transaction output", "Unmet transition output", "Unspent transmission output"],
+        1 
+    );
+
+    const question7 = new Question(
+        "What consensus algorithm does Bitcoin utilise?",
+        ["Proof of Cake","Proof of Stake","Proof of Work","Proof of Word"],
+        2
+    );
+
+    const question8 = new Question(
+        "Who are the founders of Ordinautz?",
+        ["Kluless and Hantoshi", "Fluless and Mantoshi", "Killer Queen and Darth Vader", "Peepee and Poopoo"],
+        0
+    );
+
+    const question9 = new Question (
+        "What is ordinal theory?",
+        ["A theory that proves time is a scalar quantity","Numismatic system for satoshis","Solar system for Bitcoin","A theory that proves aliens exist"],
+        1
+    );
+
+    const question10 = new Question(
+        "How many understand?",
+        ["Many","Several","Few","Everybody"],
+        2
     );
 
     await askQuestion(question1.question, question1.answersArray, question1.correctAnswerIndex);
@@ -89,14 +119,19 @@ async function main() {
     await askQuestion(question3.question, question3.answersArray, question3.correctAnswerIndex);
     await askQuestion(question4.question, question4.answersArray, question4.correctAnswerIndex);
     await askQuestion(question5.question, question5.answersArray, question5.correctAnswerIndex);
+    await askQuestion(question6.question, question6.answersArray, question6.correctAnswerIndex);
+    await askQuestion(question7.question, question7.answersArray, question7.correctAnswerIndex); 
+    await askQuestion(question8.question, question8.answersArray, question8.correctAnswerIndex); 
+    await askQuestion(question9.question, question9.answersArray, question9.correctAnswerIndex); 
+    await askQuestion(question10.question, question10.answersArray, question10.correctAnswerIndex);
     
     
     console.log(`You got ${totalCorrect} correct!`);
 
-    if (totalCorrect == 5) {
-        console.log(chalk.green("You are now an Ordinaut! Congratulations!"));
+    if (totalCorrect == 10) {
+        console.log(chalk.green("Incoming message: You are invited to join the Ordinautz on their mission. This is your password for confirmation: 'Ws are all I collect.'"));
     } else {
-        console.log(chalk.red("You need to score 5/5 to venture into space with us."));
+        console.log(chalk.red("You need to score 10/10 to venture into space with us."));
     }
 
     console.log(chalk.bgBlack(chalk.yellow('Thanks for playing!')));
